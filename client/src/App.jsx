2facf69c45920e681c957dfc8ff75918a4195fee
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-07-12 01:49:06 +0000

    feat: list all task comp.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,56 @@
+import { useState } from "react"
 import Navbar from "./components/Navbar"
 import TaskForm from "./components/task/TaskForm"
+import TaskItem from "./components/task/TaskItem";
 
 function App() {
+  const [tasks, setTasks] = useState([
+  {
+    title: "Read Book 📖",
+    description: "Finish reading chapter 3 of 'Deep Work'.",
+    resourceLink: [],
+    completed: true
+  },
+  {
+    title: "Read Docs 📕",
+    description: "Go through the ReactJS documentation on useEffect.",
+    resourceLink: ["https://reactjs.org/docs/hooks-effect.html"],
+    completed: false
+  },
+  {
+    title: "Watch Tutorial 🎥",
+    description: "Watch a YouTube tutorial on JavaScript basics.",
+    resourceLink: ["https://www.youtube.com/watch?v=W6NZfCO5SIk","https://www.youtube.com/watch?v=W6NZfCO5SIk"],
+    completed: false
+  },
+  {
+    title: "Practice Coding 💻",
+    description: "Solve 5 problems on LeetCode (Easy level).",
+    resourceLink: ["https://leetcode.com/problemset/all/"],
+    completed: false
+  },
+  {
+    title: "Write Summary 📝",
+    description: "Write a summary of today's learning in Notion.",
+    resourceLink: [],
+    completed: false
+  }
+]
+);
   return (
     <section className="p-4">
       {/* header  */}
       <Navbar />
-      <div className="flex flex-col gap-2 max-w-3xl mx-auto w-full  border border-gray-200 shadow rounded-xl my-12">
-        <h2 className="text-xl font-bold text-center mt-7">Daily Tasks {"(2/5)"} </h2>
+      <div className="flex flex-col gap-2 max-w-3xl mx-auto w-full p-4 border border-gray-200 shadow rounded-xl my-12">
+        <h2 className="text-xl font-bold text-center">Daily Tasks {"(2/5)"} </h2>
         {/* task add form  */}
         <TaskForm />
+        {/* list tasks  */}
+        {
+          tasks?.map((task,index)=>(
+         <TaskItem task={task} key={index}/>
+          ))
+        }
 
       </div>
     </section>
---
+++ client/src/components/task/TaskItem.jsx
@@ -0,0 +1,45 @@
+import { MdOutlineCheckBoxOutlineBlank, MdCheckBox } from "react-icons/md";
+import { FiTrash2, FiEdit } from "react-icons/fi";
+import { IoIosLink } from "react-icons/io";
+
+function TaskItem({ task }) {
+  return (
+    <article className="w-full border-2 border-gray-200 rounded-lg p-2 flex gap-2 mb-2 items-center  justify-between group transition-all shadow duration-150 ">
+      <div className="flex items-start gap-2 flex-1">
+        {/* checkbox  */}
+        <button type="button" className="size-">
+          {task.completed ? <MdCheckBox size={25} className="text-green-500" /> : <MdOutlineCheckBoxOutlineBlank size={25} />}
+        </button>
+        <div className="flex flex-col gap-2 p-1">
+        <h3 className="text-base font-semibold leading-2">{task.title}</h3>
+        <p className="text-sm text-gray-700">{task.description || ""}</p>
+        <div className="flex gap-2 items-center">
+          {
+                          task?.resourceLink?.map((link, index) =>
+                              <a key={index} href={link} target="_blank"
+                                  className="inline-flex text-xs transition-[max-width] duration-400 ease-in-out max-w-[9.5rem] hover:max-w-full bg-violet-500/20 text-violet-600 rounded-lg p-1 space-x-1">
+                                  <IoIosLink size={16} />
+                                  <span className="truncate w-full overflow-hidden">{link}</span>
+                              </a>
+                          )
+                      }
+        </div>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        {/* edit button  */}
+        <button type="button" className="group-hover:block hidden">
+          <FiEdit size={18} />
+        </button>
+        {/* delete button  */}
+        <button type="button" className="group-hover:block hidden ">
+          <FiTrash2 size={18} />
+        </button>
+
+      </div>
+    </article>
+
+  )
+}
+
+export default TaskItem
