5c2955916b3839255f90486225719d0dfb37414e
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-07-12 00:35:17 +0000

    refactor: update TaskForm component to initialize links and improve button visibility logic

--- client/src/components/task/TaskForm.jsx
+++ client/src/components/task/TaskForm.jsx
@@ -4,8 +4,11 @@ import ResourceLink from "./ResourceLink";
 
 
 function TaskForm() {
-    const [extended, setExtended] = useState(false);
-    const [links, setLinks] = useState([]);
+    const [extended, setExtended] = useState(true);
+    const [links, setLinks] = useState([
+        "http://google.com",
+        "http://youtube.com"
+    ]);
     return (
         <div className={`flex gap-2 w-full my-6 px-4 ${extended ? "items-start" : "items-center"}`}>
             <button
@@ -32,15 +35,22 @@ function TaskForm() {
                             className="border-2 rounded-xl w-full px-4 py-2 border-gray-300 shadow"
                         />
                         {/* links list  */}
-                        <ResourceLink links={links} />
+                        <ResourceLink links={links} setLinks={setLinks}/>
+                        {/* add button  */}
+                        <button
+                type="submit"
+                className={`bg-violet-500 border-2 border-violet-400 text-white px-4 py-2 rounded-xl font-semibold hover:bg-violet-600 duration-200 ${extended ? "":"hidden"}`}
+            >Add Task</button>
                     </>
                 }
             </div>
-
+                {
+                    !extended &&
             <button
                 type="submit"
-                className="bg-violet-500 border-2 border-violet-400 text-white px-4 py-2 rounded-xl font-semibold"
+                className={`bg-violet-500 border-2 border-violet-400 text-white px-4 py-2 rounded-xl font-semibold hover:bg-violet-600 duration-200 ${extended ? "hidden":""}`}
             >Add</button>
+                }
         </div>
     )
 }
